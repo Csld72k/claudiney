8bbc743318f5c6cae16e45d95761221ca2397f84
Claudiney Silva <csld72k@gmail.com>
2022-09-21 21:44:27 -0300

    continua a segunda parte do stage 06 do Programa Explorer da Rocketseat

---
+++ cursos/rocketseat/programa-explorer/stage-06/02-avancando-nas-promises/js/Favorites.js
@@ -0,0 +1,97 @@
+//  CLASSE QUE VAI CONTER A LÓGICA DOS DADOS;
+//  COMO OS DADOS SERÃO ESTRUTURADOS;
+export class Favorites {
+  constructor(root) {
+    this.root = document.querySelector(root);
+    this.load();
+  }
+
+  load() {
+    this.entries = [
+      {
+        login: 'maykbrito',
+        name: 'Mayk Brito',
+        public_repos: '76',
+        followers: '120000'
+      },
+      {
+        login: 'diego3g',
+        name: 'Diego Fernandes',
+        public_repos: '76',
+        followers: '120000'
+      }
+    ];
+
+  }
+
+  delete(user) {
+    const filteredEntries = this.entries.filter(entry => entry.login != user.login);
+    console.log(filteredEntries)
+  }
+
+}
+
+//  CLASSE QUE VAI CRIAR A VISUALIZAÇÃO E EVENTOS DO HTML;
+export class FavoritesView extends Favorites {
+  constructor(root) {
+    super(root);
+    this.tbody = this.root.querySelector('table tbody');
+    this.update();
+  }
+  update() {
+    this.removeAllTr();
+
+    this.entries.forEach(user => {
+      const row = this.createRow();
+
+      row.querySelector('.user img').src = `https://github.com/${user.login}.png`
+      row.querySelector('.user img').alt = `Imagem de ${user.name}`;
+      row.querySelector('.user p').textContent = user.name;
+      row.querySelector('.user span').textContent = user.login;
+      row.querySelector('.repositories').textContent = user.public_repos;
+      row.querySelector('.followers').textContent = user.followers;
+
+      row.querySelector('.remove').onclick = () => {
+        const isOk = confirm('Tem certeza que deseja deletar essa linha?');
+
+        if (isOk) {
+          this.delete(user);
+        }
+      }
+
+      this.tbody.append(row);
+    });
+
+  }
+
+  createRow() {
+    const tr = document.createElement('tr');
+
+    tr.innerHTML = `
+      <td class="user">
+        <img src="https://github.com/maykbrito.png" alt="Imagem de maykbrito">
+        <a href="https://github.com/maykbrito" target="_blank">
+          <p>Mayk Brito</p>
+          <span>maykbrito</span>
+        </a>
+      </td>
+      <td class="repositories">
+        76
+      </td>
+      <td class="followers">
+        9589
+      </td>
+      <td>
+        <button class="remove">&times;</button>
+      </td>
+    `
+
+    return tr
+  }
+
+  removeAllTr() {
+    this.tbody.querySelectorAll('tr').forEach((tr) => {
+      tr.remove();
+    });
+  }
+}
