64a7caedaaad4bb63ed66651212bab36a00cd212
Claudiney Silva <csld72k@gmail.com>
2022-09-22 22:33:27 -0300

    finaliza o projeto 03 do stage 06 do Programa Explorer da Rocketseat

--- cursos/rocketseat/programa-explorer/stage-06/02-avancando-nas-promises/js/Favorites.js
+++ cursos/rocketseat/programa-explorer/stage-06/02-avancando-nas-promises/js/Favorites.js
@@ -1,32 +1,55 @@
+import { GithubUser } from "./GithubUser.js";
+
 //  CLASSE QUE VAI CONTER A LÓGICA DOS DADOS;
 //  COMO OS DADOS SERÃO ESTRUTURADOS;
 export class Favorites {
   constructor(root) {
     this.root = document.querySelector(root);
     this.load();
+
+
   }
 
   load() {
-    this.entries = [
-      {
-        login: 'maykbrito',
-        name: 'Mayk Brito',
-        public_repos: '76',
-        followers: '120000'
-      },
-      {
-        login: 'diego3g',
-        name: 'Diego Fernandes',
-        public_repos: '76',
-        followers: '120000'
-      }
-    ];
+    this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || [];
 
+  }
+
+  save() {
+
+    localStorage.setItem('@github-favorites:', JSON.stringify(this.entries));
+
+  }
+
+  async add(username) {
+
+    try {
+
+      const userExists = this.entries.find(entry => entry.login === username);
+
+      if (userExists) throw new Error('Usuário já cadastrado!');
+
+      const user = await GithubUser.search(username);
+      console.log(user);
+
+      if (user.login === undefined) throw new Error('Usuário não encontrado!');
+
+      this.entries = [user, ...this.entries];
+      this.update();
+      this.save();
+
+    } catch (error) {
+
+      alert(error.message)
+
+    }
   }
 
   delete(user) {
     const filteredEntries = this.entries.filter(entry => entry.login != user.login);
-    console.log(filteredEntries)
+    this.entries = filteredEntries;
+    this.update();
+    this.save();
   }
 
 }
@@ -37,7 +60,17 @@ export class FavoritesView extends Favorites {
     super(root);
     this.tbody = this.root.querySelector('table tbody');
     this.update();
+    this.onadd();
   }
+
+  onadd() {
+    const addButton = this.root.querySelector('.search button');
+    addButton.onclick = () => {
+      const { value } = this.root.querySelector('.search input')
+      this.add(value);
+    }
+  }
+
   update() {
     this.removeAllTr();
 
@@ -46,6 +79,7 @@ export class FavoritesView extends Favorites {
 
       row.querySelector('.user img').src = `https://github.com/${user.login}.png`
       row.querySelector('.user img').alt = `Imagem de ${user.name}`;
+      row.querySelector('.user a').href = `https://github.com/${user.login}`;
       row.querySelector('.user p').textContent = user.name;
       row.querySelector('.user span').textContent = user.login;
       row.querySelector('.repositories').textContent = user.public_repos;
