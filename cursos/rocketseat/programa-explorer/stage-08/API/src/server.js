2bd3a18097bf374cf7a531660c420797e89058b0
Claudiney <csld72k@gmail.com>
2022-11-23 20:39:14 -0300

    finaliza o módulo de NodeJS do Programa Explorer da Rocketseat

--- cursos/rocketseat/programa-explorer/stage-08/API/src/server.js
+++ cursos/rocketseat/programa-explorer/stage-08/API/src/server.js
@@ -1,13 +1,31 @@
-const { response } = require('express')
-const express = require('express')
+require("express-async-errors")
+
+const AppError = require("./utils/AppError")
+
+const express = require("express")
+
+const routes = require("./routes")
 
 const app = express()
 app.use(express.json())
 
-app.post("/users", (request, response) => {
-  const { name, email, password } = request.body
+app.use(routes)
 
-  response.json({ name, email, password })
+app.use((error, request, response, next) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message
+    })
+  }
+
+  console.error(error)
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error"
+  })
+
 })
 
 const PORT = 3333
