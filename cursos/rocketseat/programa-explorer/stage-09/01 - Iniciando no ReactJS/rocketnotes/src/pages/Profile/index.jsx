f2f33f129e547087b5527f71a298355a8ce2985a
Claudiney <csld72k@gmail.com>
2023-01-12 21:20:22 -0300

    Insere a foto de perfil e input para troca de foto de perfil da página 'Prfile'

--- cursos/rocketseat/programa-explorer/stage-09/01 - Iniciando no ReactJS/rocketnotes/src/pages/Profile/index.jsx
+++ cursos/rocketseat/programa-explorer/stage-09/01 - Iniciando no ReactJS/rocketnotes/src/pages/Profile/index.jsx
@@ -1,7 +1,7 @@
-import { FiArrowLeft, FiLock, FiMail, FiUser } from "react-icons/fi";
+import { FiArrowLeft, FiCamera, FiLock, FiMail, FiUser } from "react-icons/fi";
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
-import { Container, Form } from "./style";
+import { Avatar, Container, Form } from "./style";
 
 
 export function Profile() {
@@ -14,6 +14,17 @@ export function Profile() {
       </header>
 
       <Form>
+        <Avatar>
+          <img src="https://github.com/csld72k.png" alt="Foto do usuário" />
+
+          <label htmlFor="avatar">
+            <FiCamera />
+
+            <input id="avatar" type="file" />
+
+          </label>
+        </Avatar>
+
         <Input placeholder="Nome" type="text" icon={FiUser} />
 
         <Input placeholder="E-mail" type="text" icon={FiMail} />
