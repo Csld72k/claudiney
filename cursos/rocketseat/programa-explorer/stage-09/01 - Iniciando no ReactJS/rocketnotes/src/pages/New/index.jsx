083532f494dbf58a443409c4eb73cd2ee6be1667
Claudiney <csld72k@gmail.com>
2023-01-12 21:56:21 -0300

    Cria uma interface para cadastro de notas na aplicação

---
+++ cursos/rocketseat/programa-explorer/stage-09/01 - Iniciando no ReactJS/rocketnotes/src/pages/New/index.jsx
@@ -0,0 +1,27 @@
+import { Header } from "../../components/Header";
+import { Input } from "../../components/Input";
+import { TextArea } from "../../components/TextArea";
+import { Container, Form } from "./style";
+
+export function New() {
+  return (
+    <Container>
+      <Header />
+
+      <main>
+        <Form>
+          <header>
+            <h1>Criar nota</h1>
+            <a href="/">Voltar</a>
+          </header>
+
+          <Input placeholder="Título" />
+
+          <TextArea placeholder="Observações" />
+
+        </Form>
+      </main>
+
+    </Container>
+  );
+}
