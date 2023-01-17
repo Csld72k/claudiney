eb54fcd83d4f54e7997d823318d508d4395c93c6
Claudiney <csld72k@gmail.com>
2023-01-17 18:00:24 -0300

    Finaliza a página de Profile da aplicação RocketMovies

--- cursos/rocketseat/programa-explorer/stage-09/01 - Iniciando no ReactJS/02-rocketmovies/src/pages/Profile/index.jsx
+++ cursos/rocketseat/programa-explorer/stage-09/01 - Iniciando no ReactJS/02-rocketmovies/src/pages/Profile/index.jsx
@@ -25,11 +25,13 @@ export function Profile() {
           </label>
         </Avatar>
 
-        <Input type="text" placeholder="Nome" icon={FiUser} />
-        <Input type="text" placeholder="E-mail" icon={FiMail} />
-        <Input type="password" placeholder="Senha atual" icon={FiLock} />
-        <Input type="password" placeholder="Nova senha" icon={FiLock} />
-        <Button title="Salvar" />
+        <div className="inputContainer">
+          <Input type="text" placeholder="Nome" icon={FiUser} />
+          <Input type="text" placeholder="E-mail" icon={FiMail} />
+          <Input type="password" placeholder="Senha atual" icon={FiLock} />
+          <Input type="password" placeholder="Nova senha" icon={FiLock} />
+          <Button title="Salvar" />
+        </div>
 
       </div>
     </Container>
--- cursos/rocketseat/programa-explorer/stage-09/01 - Iniciando no ReactJS/02-rocketmovies/src/pages/Profile/style.js
+++ cursos/rocketseat/programa-explorer/stage-09/01 - Iniciando no ReactJS/02-rocketmovies/src/pages/Profile/style.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  height: 100vh;
+
   background-color: ${({ theme }) => theme.colors.background};
 
   > header {
@@ -29,28 +31,66 @@ export const Container = styled.div`
     justify-content: center;
     align-items: center;
 
-    
+    div.inputContainer {
+      display: flex;
+      flex-direction: column;
+      gap: .8rem;
+
+      /* &:nth-child(4) {
+        display: none;
+        margin-top: 1.6rem;
+      } */
+
+      div.input {
+        width: 34rem;
+        height: 5.6rem;
+
+        &:nth-child(3) {
+        margin-top: 1.6rem;
+      }
+
+        }
+          
+      button {
+        margin-top: 2.4rem;
+
+        font-size: 1.6rem;
+        font-weight: 500;
+        
+        color: ${({ theme }) => theme.colors.tag_background_dark};
+      }
+    }
   }
 `;
 
 export const Avatar = styled.div`
+  margin-bottom: 6.4rem;
+  position: relative;
 
   > img {
     width: 18.6rem;
+    margin-top: -9rem;
   }
   
   > label { 
     width: 4.8rem;
     height: 4.8rem;
+    position: absolute;
+    bottom: .7rem;
+    right: .7rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
     background-color: ${({ theme }) => theme.colors.primary_color};
     border-radius: 50%;
     
     svg {
       color: ${({ theme }) => theme.colors.tag_background_dark};
+    }
 
-      /* border: 4px solid ${({ theme }) => theme.colors.primary_color}; */
-      border-radius: 50%;
-  }
+    div {
+      display: none;
+    }
 }
 `;
