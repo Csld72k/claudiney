3b2b13c872869598ebe71674aa8869ab2bdc1c85
Claudiney <csld72k@gmail.com>
2023-01-24 21:29:26 -0300

    Finaliza a página de notas do filme da aplicação RocketMovies do stage 09 do Programa Explorer da Rocketseat

--- cursos/rocketseat/programa-explorer/stage-09/01 - Iniciando no ReactJS/02-rocketmovies/src/pages/MovieDetails/index.jsx
+++ cursos/rocketseat/programa-explorer/stage-09/01 - Iniciando no ReactJS/02-rocketmovies/src/pages/MovieDetails/index.jsx
@@ -1,7 +1,7 @@
 import { Container } from "./style";
 import { Header } from "../../components/Header";
 import { ButtonText } from "../../components/ButtonText";
-import { Stack } from "../../components/Stack";
+import { Tag } from "../../components/Tag";
 import { FiArrowLeft, FiClock, FiStar } from "react-icons/fi";
 
 export function MovieDetails() {
@@ -12,7 +12,7 @@ export function MovieDetails() {
         <div className="info">
           <ButtonText title="Voltar" icon={FiArrowLeft} />
 
-          <div>
+          <div className="title">
             <h1>Interstellar</h1>
             <div className="rating">
               {<FiStar className="star1 filled" size={20} />}
@@ -23,14 +23,27 @@ export function MovieDetails() {
             </div>
           </div>
 
-          <span>
+          <div className="postInfo">
             <img src="https://github.com/csld72k.png" alt="Imagem de perfil." />
             <p>Por Claudiney Silva</p>
-            <FiClock />
+            <FiClock size={16} />
             <p>23/01/2023 às 08:00</p>
-          </span>
+          </div>
 
+          <div className="tags">
+            <Tag isActive title="Ficção científica" />
+            <Tag isActive title="Drama" />
+            <Tag isActive title="Família" />
+          </div>
 
+          <div className="notes">
+            <p>
+              Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se comunicar com ela. Pai e filha descobrem que o "fantasma" é uma inteligência desconhecida que está enviando mensagens codificadas através de radiação gravitacional, deixando coordenadas em binário que os levam até uma instalação secreta da NASA liderada pelo professor John Brand. O cientista revela que um buraco de minhoca foi aberto perto de Saturno e que ele leva a planetas que podem oferecer condições de sobrevivência para a espécie humana. As "missões Lázaro" enviadas anos antes identificaram três planetas potencialmente habitáveis orbitando o buraco negro Gargântua: Miller, Edmunds e Mann – nomeados em homenagem aos astronautas que os pesquisaram. Brand recruta Cooper para pilotar a nave espacial Endurance e recuperar os dados dos astronautas; se um dos planetas se mostrar habitável, a humanidade irá seguir para ele na instalação da NASA, que é na realidade uma enorme estação espacial. A partida de Cooper devasta Murphy.
+            </p>
+            <p>
+              Além de Cooper, a tripulação da Endurance é formada pela bióloga Amelia, filha de Brand; o cientista Romilly, o físico planetário Doyle, além dos robôs TARS e CASE. Eles entram no buraco de minhoca e se dirigem a Miller, porém descobrem que o planeta possui enorme dilatação gravitacional temporal por estar tão perto de Gargântua: cada hora na superfície equivale a sete anos na Terra. Eles entram em Miller e descobrem que é inóspito já que é coberto por um oceano raso e agitado por ondas enormes. Uma onda atinge a tripulação enquanto Amelia tenta recuperar os dados de Miller, matando Doyle e atrasando a partida. Ao voltarem para a Endurance, Cooper e Amelia descobrem que 23 anos se passaram.
+            </p>
+          </div>
 
         </div>
       </div>
--- cursos/rocketseat/programa-explorer/stage-09/01 - Iniciando no ReactJS/02-rocketmovies/src/pages/MovieDetails/style.js
+++ cursos/rocketseat/programa-explorer/stage-09/01 - Iniciando no ReactJS/02-rocketmovies/src/pages/MovieDetails/style.js
@@ -1,5 +1,95 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: grid;
+  grid-template-rows: min-content auto;
+  grid-template-areas: 
+  "header"
+  "content";
+  
   background-color: ${({ theme }) => theme.colors.background};
+
+  header {
+    grid-area: header;
+  }
+  
+  > .container {
+    width: 122rem;
+    margin: 0 auto;
+    grid-area: content;
+    overflow-y: auto;
+
+    a[type=button] {
+      margin: 4rem 0 2.4rem;
+      display: flex;
+    }
+
+    .title {
+      height: 100%;
+      display: flex;
+      align-items: center;
+    
+      h1 {
+        font-size: 3.6rem;
+        font-weight: 500;
+        
+        color: ${({ theme }) => theme.colors.white_light};
+      }
+
+      .rating {
+        margin: .9rem 0 0 2rem;
+      }
+    }
+
+    .postInfo {
+      margin-top: 2.4rem;
+      display: flex;
+      align-items: center;
+
+      color: ${({ theme }) => theme.colors.white_light};
+
+      img {
+        width: 1.6rem;
+        margin-right: .8rem;
+      }
+
+      p {
+        font-size: 1.6rem;
+        font-weight: 400;
+      }
+
+      svg {
+        margin: 0 .8rem;
+        
+        color: ${({ theme }) => theme.colors.primary_color};
+      }
+    }
+
+    .tags {
+      margin-top: 4rem;
+      display: flex;
+      gap: .8rem;
+
+      div {
+        background-color: ${({ theme }) => theme.colors.tag_background};
+      }
+    }
+
+    .notes {     
+      margin-top: 4rem;
+      display: flex;
+      flex-direction: column;
+      gap: 1.6rem;
+
+      
+      font-size: 1.6rem;
+      font-weight: 400;
+      text-align: justify;
+      
+      color: ${({ theme }) => theme.colors.white_light};
+    }
+
+  }
 `;
