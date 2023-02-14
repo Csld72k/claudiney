0c0a22f81cba65951864dc3997864b506d43a7cb
Claudiney <csld72k@gmail.com>
2023-02-14 14:59:07 -0300

    Implementa e testa o método GET à aplicação

--- cursos/rocketseat/programa-explorer/stage-08/API - Remade/src/server.js
+++ cursos/rocketseat/programa-explorer/stage-08/API - Remade/src/server.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (request, response) => {
+  response.send("Hello World!");
+});
+
 const PORT = 3333;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
