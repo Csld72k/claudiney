9fc1adc706a31c943ca9193c4be84cfe84571a98
Claudiney <csld72k@gmail.com>
2023-02-15 17:34:39 -0300

    Aprendendo sobre Query Params

--- cursos/rocketseat/programa-explorer/stage-08/API - Remade/src/server.js
+++ cursos/rocketseat/programa-explorer/stage-08/API - Remade/src/server.js
@@ -4,8 +4,15 @@ const app = express();
 
 app.get("/message/:id/:user", (request, response) => {
   const { id, user } = request.params;
+
   response.send(`Id da mensagem: ${id} para o usuário: ${user}`);
 });
+
+app.get("/users", (request, response) => {
+  const { page, limit } = request.query;
+
+  response.send(`Página: ${page}. Mostrar: ${limit}`);
+})
 
 const PORT = 3333;
 
