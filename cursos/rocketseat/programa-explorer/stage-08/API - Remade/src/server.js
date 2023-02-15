b266aec21799a290a85395d4b201ed7d0f0ee5d6
Claudiney <csld72k@gmail.com>
2023-02-15 18:32:49 -0300

    Aprendendo sobre método POST e Body Params, instalando e usando o Insomnia para utilizar o método POST

--- cursos/rocketseat/programa-explorer/stage-08/API - Remade/src/server.js
+++ cursos/rocketseat/programa-explorer/stage-08/API - Remade/src/server.js
@@ -1,17 +1,12 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
-app.get("/message/:id/:user", (request, response) => {
-  const { id, user } = request.params;
+app.post("/users", (request, response) => {
+  const { name, email, password } = request.body;
 
-  response.send(`Id da mensagem: ${id} para o usuário: ${user}`);
-});
-
-app.get("/users", (request, response) => {
-  const { page, limit } = request.query;
-
-  response.send(`Página: ${page}. Mostrar: ${limit}`);
+  response.json({ name, email, password });
 })
 
 const PORT = 3333;
