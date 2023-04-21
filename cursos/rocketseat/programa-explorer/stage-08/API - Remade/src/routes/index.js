9fc5f4976874fb8cf1c62bd871cfdc18c35ccae9
Claudiney <csld72k@gmail.com>
2023-04-21 14:10:34 -0300

    deleta o conteúdo do stage 08 do Programa Explorer da Rocketseat para refazê-lo

--- cursos/rocketseat/programa-explorer/stage-08/API - Remade/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-
-const usersRouter = require("./users.routes");
-
-const routes = Router();
-
-routes.use("/users", usersRouter);
-
-module.exports = routes;
