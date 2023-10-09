7b487923031a82ede3cbf7c68fab7e1543818638
Claudiney <csld72k@gmail.com>
2023-10-08 21:52:39 -0300

    New project

--- cursos/rocketseat/programa-explorer/stage-12 desafio final/food-explorer-frontend/src/services/api.js
+++ cursos/rocketseat/programa-explorer/stage-12 desafio final/food-explorer-frontend/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://food-explorer-api-b1an.onrender.com"
+  baseURL: "https://food-explorer-api-26jk.onrender.com"
 });
